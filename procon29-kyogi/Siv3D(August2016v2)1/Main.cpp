d95ec65ebce25ab1e3abe534d8c47ac00aa690a8
yato0421 <raizaraiza777@gmail.com>
2018-08-19 22:40:54 +0900

    総当たりを実行するかのトリガーを追加

--- procon29-kyogi/Siv3D(August2016v2)1/Main.cpp
+++ procon29-kyogi/Siv3D(August2016v2)1/Main.cpp
@@ -6,9 +6,7 @@ void Main()
 	const Font font(30);
 	while (System::Update())
 	{
-		Map *hoge = hoge->getMap();
-		hoge->Turn = 10;
-		font(hoge->Turn).draw();
+		font(L"ようこそ、Siv3D の世界へ！").draw();
 		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
 	}
 }
