d68cc6d3e6faa70484aaf4e4be7972e94956f7d3
Kaito Sato <ksato.nit@gmail.com>
2018-10-08 22:07:43 +0900

    謎のヘルパー関数を書いた

--- procon29-kyogi/Siv3D(August2016v2)1/IOManager/CreateMapClass.cpp
+++ procon29-kyogi/Siv3D(August2016v2)1/IOManager/CreateMapClass.cpp
@@ -2,10 +2,23 @@
 
 void CreateMapClass::createMapClass(void)
 {
+	ReadQR rqr;
+	input = rqr.readQR(); //クラス変数 input に読み取った内容を代入．
+
 	Map *map;
 	map = map->getMap();
 
+	createMasuClass();
+	createAgent();
+}
+
+void CreateMapClass::createMasuClass(void)
+{
+	//読み取った内容をもとに各マスクラスを作成
 	//Masuを代入
+	Map *map;
+	map = map->getMap();
+
 	for (int j = 0; j < VERTICAL; ++j) {
 		for (int i = 0; i < WIDTH; ++i) {
 			Masu masu;
@@ -13,15 +26,52 @@ void CreateMapClass::createMapClass(void)
 			map->board[j][i] = masu;
 		}
 	}
-	//Agentクラス,Masuクラスをまとめる配列を用意し代入
-}
-
-void CreateMapClass::createMasuClass(void)
-{
-	//読み取った内容をもと各エージェントクラスを作成
 }
 
 void CreateMapClass::createAgent(void)
 {
-	//読み取った内容をもとに各マスクラスを作成
+	//読み取った内容をもとに各エージェントクラスを作成
+}
+
+//文字列を : で分割し，vector<std::string> に突っ込んで返す．
+//ヘルパー関数みたいなものです．さしたる意味はないです．
+vector<std::string> CreateMapClass::splitStringByCoron(const std::string &s)
+{
+	vector<string> elems;
+	string item;
+	for (char ch : s) {
+		if (ch == ':') {
+			if (!item.empty())
+				elems.push_back(item);
+			item.clear();
+		}
+		else {
+			item += ch;
+		}
+	}
+	if (!item.empty())
+		elems.push_back(item);
+	return elems;
+}
+
+//文字列を半角スペースで分割し，vector<int> を返す．
+//上と同じくヘルパー関数です．
+vector<int> CreateMapClass::splitStringBySpace(const std::string &s)
+{
+	vector<int> elems;
+	string item;
+	for (char ch : s) {
+		if (ch == ' ') {
+			if (!item.empty()) {
+				elems.push_back(stoi(item));
+			}
+			item.clear();
+		}
+		else {
+			item += ch;
+		}
+	}
+	if (!item.empty())
+		elems.push_back(stoi(item));
+	return elems;
 }
--- procon29-kyogi/Siv3D(August2016v2)1/IOManager/CreateMapClass.h
+++ procon29-kyogi/Siv3D(August2016v2)1/IOManager/CreateMapClass.h
@@ -2,11 +2,15 @@
 #include "ReadQR.h"
 #include "../Data/Map.h"
 #include "../Data/Masu.h"
+#include "string"
 
 class CreateMapClass {
 public:
 	void createMapClass(void);
+	static std::vector<string> splitStringByCoron(const std::string &s);
+	static std::vector<int> splitStringBySpace(const std::string &s);
 private:
 	void createMasuClass(void);
 	void createAgent(void);
+	std::string input;
 };
