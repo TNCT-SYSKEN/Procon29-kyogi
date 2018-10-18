afecfd4fb066ca2d1063290f21f73c25bff8edeb
yato-octopus <miyake235711@gmail.com>
2018-10-19 02:33:06 +0900

    agentの行動状態を保存する変数を追加

--- procon29-kyogi/Siv3D(August2016v2)1/Data/Agent.h
+++ procon29-kyogi/Siv3D(August2016v2)1/Data/Agent.h
@@ -4,9 +4,21 @@
 class Agent {
 public :
 	//エージェントがそのターンにおいて動くべき最終的な座標
-	pair <int, int> lastPosition = make_pair(0, 0);
+	pair <int, int> nextPosition;
+	//エージェントが除去するタイルの座標
+	pair <int, int> erasePosition;
 	//エージェントの現在位置
 	pair <int, int> position;
+	//エージェントが行う行動
+	enum actAgent{
+		//停滞
+		stagnation = 0,
+		//移動
+		move = 1,
+		//タイル除去
+		erase = 2
+	};
+	actAgent actAgent;
 	//どのエージェントか
 	enum EnemyNumber { friend1 = 0, friend2 = 1, enemy1 = 3, enemy2 = 4 };
 	EnemyNumber Status;
