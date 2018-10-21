d6426a5e7710ae8ba0dc018543b16faaf41bda56
Kaito Sato <ksato.nit@gmail.com>
2018-10-21 13:07:32 +0900

    タイルポイント計算処理を書いた

--- procon29-kyogi/Siv3D(August2016v2)1/Algorithm/BruteForce.cpp
+++ procon29-kyogi/Siv3D(August2016v2)1/Algorithm/BruteForce.cpp
@@ -1,7 +1,23 @@
 #include "BruteForce.h"
 
+int dy[] = { 1, 0, -1, 0 , 1, 1, -1, -1 };
+int dx[] = { 0, 1, 0, -1 , 1, -1, 1, -1 };
+
 void BruteForce::bruteForce(void)
 {
-	//総当たりによって最も良い手を決める
-	//戻り値は最善手をPairで返す？
+	//経路のパターンを生やす．
+
+	//残りターン数．現在はダミー
+	int remainingTurn = 3;
+}
+
+//経路 route を取った際のスコアを計算する．
+int BruteForce::calculateScore(int nowX, int nowY, vector<int> route) {
+	int tileScore = 0;
+	Map *map;
+	map = map->getMap();
+	for (int i : route) {
+		nowX += dx[i]; nowY += dy[i];
+		tileScore += map->board[nowY][nowX].TilePoint;
+	}
 }
--- procon29-kyogi/Siv3D(August2016v2)1/Algorithm/BruteForce.h
+++ procon29-kyogi/Siv3D(August2016v2)1/Algorithm/BruteForce.h
@@ -1,6 +1,12 @@
 #pragma once
+#include "../gneral.h"
+#include "../Data/Map.h"
+
 class BruteForce {
 public:
 	void bruteForce(void);
 private:
+	int calculateScore(int, int, vector<int>) {
+
+	}
 };
