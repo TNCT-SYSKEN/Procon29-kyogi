a5740f378a482183e275278d75f1743dc36a81c6
yato-octopus <miyake235711@gmail.com>
2018-10-23 21:20:40 +0900

    自分のマスに移動できないバグを修正＆停滞意思の追加

--- procon29-kyogi/Siv3D(August2016v2)1/AgentManager.cpp
+++ procon29-kyogi/Siv3D(August2016v2)1/AgentManager.cpp
@@ -42,7 +42,7 @@ void AgentManager::checkAgentConflict()
 
 	int count = 0;
 
-	//nextPosによって移動できるどうか判断
+	//nextPosによって移動できるどうか判断ok
 	for (int i = 0; i < AGENTS; i++) {
 		for (int j = 0; j < AGENTS; j++) {
 			//agent同士のnextPosを比較し、同じならcount++;
@@ -53,16 +53,17 @@ void AgentManager::checkAgentConflict()
 		}
 		//最低一回は自分自身のnextPosと比較するため、count >=2ならばtrue
 		if (count >= 2) {
-			map->agents[i].canMoveNextPos = false;
+			map->agents[i].canMoveNextPos = true;
 		}
 		else {
-			map->agents[i].canMoveNextPos = true;
+			map->agents[i].canMoveNextPos = false;
 		}
 		count = 0;
 	}
 
-	//移動先に敵のエージェントがいるかどうかの判定
+	//移動先にエージェントがいるかどうかの判定ok
 	for (int i = 0; i < AGENTS; i++) {
+		map->agents[i].beAgent = false;
 		for (int j = 0; j < AGENTS; j++) {
 			if (i != j) {
 				//agent同士のnextPosを比較し、同じならcount++;
@@ -72,14 +73,11 @@ void AgentManager::checkAgentConflict()
 					map->agents[i].beAgentNum =j;
 					break;
 				}
-				else {
-					map->agents[i].beAgent = false;
-				}
 			}
 		}
 	}
 
-	//タイル状況によって移動できるか判定
+	//タイル状況によって移動できるか判定ok
 	for (int i = 0; i < AGENTS; i++) {
 		//味方のエージェントの場合
 		if (0 == i || 1 == i) {
@@ -102,6 +100,17 @@ void AgentManager::checkAgentConflict()
 			}
 		}
 	}
+
+	//停滞を意思しているかどうかok
+	for (int i = 0; i < AGENTS; i++){
+		if (map->agents[i].position.first == map->agents[i].nextPosition.first &&
+			map->agents[i].position.second == map->agents[i].nextPosition.second) {
+			map->agents[i].doStagnation = true;
+		}
+		else {
+			map->agents[i].doStagnation = false;
+		}
+	}
 }
 
 //エージェントの行う動きを決定
@@ -116,9 +125,12 @@ void AgentManager::decideAgentAct()
 	for (int i = 0; i < AGENTS; i++) {
 		//移動先が被ってない
 		//移動先に相手のタイルがない
-		if (map->agents[i].canMoveNextPos == true &&
+		//移動先にエージェントがいない
+		//停滞意思表示をしていない
+		if (map->agents[i].canMoveNextPos == false &&
 			map->agents[i].canMoveTile == false&&
-			map->agents[i].beAgent == false) {
+			map->agents[i].beAgent == false&&
+			map->agents[i].doStagnation == false) {
 			map->agents[i].actAgent = Agent::move;
 		}
 	}
@@ -150,12 +162,14 @@ void AgentManager::decideAgentAct()
 	*/
 	//停滞かどうか判定
 	for (int i = 0; i < AGENTS;i++) {
-		if (map->agents[i].position.first == map->agents[i].nextPosition.first &&
-			map->agents[i].position.second == map->agents[i].nextPosition.second||
-			map->agents[i].beAgent == true||
-			map->agents[i].canMoveNextPos == false ||
-			map->agents[i].canMoveTile == true
-			) {
+		//移動先が被っている
+		//移動先に相手のタイルがある
+		//移動先にエージェントがいる
+		//停滞意思表示をしている
+		if (map->agents[i].beAgent == true||
+			map->agents[i].canMoveNextPos == true||
+			map->agents[i].canMoveTile == true||
+			map->agents[i].doStagnation == true) {
 			//停滞：stagnation
 			map->agents[i].actAgent = Agent::stagnation;
 		}
