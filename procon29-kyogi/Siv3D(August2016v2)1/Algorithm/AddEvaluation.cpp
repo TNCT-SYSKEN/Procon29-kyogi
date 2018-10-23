2e40983b7bd29311a8d59d1621201891e4c546fa
Kaito Sato <ksato.nit@gmail.com>
2018-10-23 19:20:06 +0900

    タイル置いてある場所を手に含めないようにした

--- procon29-kyogi/Siv3D(August2016v2)1/Algorithm/AddEvaluation.cpp
+++ procon29-kyogi/Siv3D(August2016v2)1/Algorithm/AddEvaluation.cpp
@@ -8,7 +8,8 @@ pair<int, int> AddEvaluation::addEvaluation(Agent agent)
 	//ここで8近傍の各点について評価点を求め，Evaluationに突っ込む．順番は以下の配列に従う．
 	int dy[] = { 1, 0, -1, 0 , 1, 1, -1, -1};
 	int dx[] = { 0, 1, 0, -1 , 1, -1, 1, -1 };
-	Evaluation evl;
+	Evaluation *evl;
+	evl = evl->getEvaluation();
 
 	for (int i = 0; i < 16; ++i) {
 		Prefetching prefetching;
@@ -17,9 +18,9 @@ pair<int, int> AddEvaluation::addEvaluation(Agent agent)
 		Map *map;
 		map = map->getMap();
 		if (newX >= 0 && newX < map->Width && newY >= 0 && newY < map->Vertical) {
-			if ((map->board[newY][newX].Status == Masu::EnemyTile && i >= 8) || (map->board[newY][newX].Status != Masu::EnemyTile && i < 8)) {
+			if ((map->board[newY][newX].Status == Masu::EnemyTile && i >= 8) || (map->board[newY][newX].Status == Masu::Non && i < 8)) {
 				vector<int> ret = prefetching.prefetching(newX, newY, i >= 8);
-				evl.SumScore[i] = ret[0]; evl.TileScore[i] = ret[1]; evl.Movable[i] = ret[2];
+				evl->SumScore[i] = ret[0]; evl->TileScore[i] = ret[1]; evl->Movable[i] = ret[2];
 			}
 		}
 		JudgmentEncircle judgeEncircle;
@@ -28,7 +29,7 @@ pair<int, int> AddEvaluation::addEvaluation(Agent agent)
 
 	//Evaluationを受けて評価し，最適な手を出す．
 	Judgment judge;
-	pair<int, int> ansPosition = judge.judgment(evl);
+	pair<int, int> ansPosition = judge.judgment(*evl);
 
 	return make_pair(agent.position.first + ansPosition.first , agent.position.second + ansPosition.second);
 }
