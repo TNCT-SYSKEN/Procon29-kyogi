4cc8878398f1879cb936842a184787ba0a498bfd
Kaito Sato <ksato.nit@gmail.com>
2019-03-08 13:56:44 +0900

    総当たりを諦めた

--- procon29-kyogi/Siv3D(August2016v2)1/Algorithm/AlgorithmManager.cpp
+++ procon29-kyogi/Siv3D(August2016v2)1/Algorithm/AlgorithmManager.cpp
@@ -8,34 +8,18 @@ void AlgorithmManager::algorithmManager(void)
 	Setting *setting;
 	setting = setting->getSetting();
 
-	if (setting->bruteForce) {
-		Map *map;
-		map = map->getMap();
+	Map *map;
+	map = map->getMap();
 
-		BruteForce bruteForce;
+	AddEvaluation addEvaluation;
 
-		Agent agent1 = map->agents[0];
-		setting->bruteForceResults.push_back(bruteForce.bruteForce(agent1.position.second, agent1.position.first));
+	Agent agent1 = map->agents[0];
+	map->agents[0].nextPosition = addEvaluation.addEvaluation(agent1, 1);
 
-		Agent agent2 = map->agents[1];
-		setting->bruteForceResults.push_back(bruteForce.bruteForce(agent2.position.second, agent2.position.first));
-	} else {
-		Map *map;
-		map = map->getMap();
+	Println();
 
-		AddEvaluation addEvaluation;
+	Agent agent2 = map->agents[1];
+	map->agents[1].nextPosition = addEvaluation.addEvaluation(agent2, 2);
 
-		Agent agent1 = map->agents[0];
-		map->agents[0].nextPosition = addEvaluation.addEvaluation(agent1, 1);
-
-		Println();
-
-		Agent agent2 = map->agents[1];
-		map->agents[1].nextPosition = addEvaluation.addEvaluation(agent2, 2);
-
-		Println();
-
-		//Println(map->agents[0].nextPosition.first, L" ", map->agents[0].nextPosition.second);
-		//Println(map->agents[1].nextPosition.first, L" ", map->agents[1].nextPosition.second);
-	}
+	Println();
 }
